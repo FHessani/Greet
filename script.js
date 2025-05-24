0f1ae369ed70de999f9324cda296890fa3fdfc91
Farhan <Farhanessani704gmail.com>
2025-05-24 18:00:43 +0500

    jscorrect

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function greetUser() {
-      var name = document.getElementById("nameInput").value;
-      var greetingText = "Hello, " + name + "!";
+      const name = document.getElementById("nameInput").value;
+      const greetingText = "Hello, " + name + "!";
       document.getElementById("greeting").innerText = greetingText;
     }
 
     function chbg() {
-        var red = [Math.round(Math.random() * 256)];
-        var green = [Math.round(Math.random() * 256)];
-        var blue = [Math.round(Math.random() * 256)];
-        var random = ("rgb(" + red + "," + green + "," + blue + ")");
+        const red = [Math.round(Math.random() * 256)];
+        const green = [Math.round(Math.random() * 256)];
+        const blue = [Math.round(Math.random() * 256)];
+        const random = ("rgb(" + red + "," + green + "," + blue + ")");
         document.body.style.backgroundColor = random;
     }
